aaf1749a7f88dd149b5b55e64f8976d2844469f2
tuanhq.ront <tuanhq@edsolabs.com>
2022-10-07 17:09:43 +0700

    test build

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+## This [Demo](https://learning-nextjs-ts.vercel.app/)
+
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
 ## Getting Started
--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href='https://nextjs.org'>Next.js!</a>
+          Welcome to <a href='https://nextjs.org'>tuanhq</a>
         </h1>
 
         <p className={styles.description}>
@@ -24,7 +24,7 @@ const Home: NextPage = () => {
         <div className={styles.grid}>
           <a href='https://nextjs.org/docs' className={styles.card}>
             <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
+            <p>tuanhq</p>
           </a>
 
           <a href='https://nextjs.org/learn' className={styles.card}>
